66aaf2ade5a72363f9d080c3bd416059711802c0
Marcin Dudek <manekd@gmail.com>
2024-10-15 07:58:44 +0000

    Add selection highlighting

--- src/routes/akwarela/+page.svelte
+++ src/routes/akwarela/+page.svelte
@@ -1,24 +1,28 @@
 <script>
+  import { page } from '$app/stores';
+
   let files = [
-    './img_a/w101.webp',
-    './img_a/w102.webp',
-    './img_a/w103.webp',
-    './img_a/w105.webp',
-    './img_a/w106.webp',
-    './img_a/w107.webp',
-    './img_a/w108.webp',
-    './img_a/w109.webp',
-    './img_a/w110.webp',
-    './img_a/w111.webp',
-    './img_a/w112.webp',
-    './img_a/w113.webp',
-    './img_a/w114.webp',
-    './img_a/w115.webp',
-    './img_a/w116.webp',
-    './img_a/w117.webp',
-    './img_a/w118.webp',
-    './img_a/w119.webp'
+    '/img_a/w101.webp',
+    '/img_a/w102.webp',
+    '/img_a/w103.webp',
+    '/img_a/w105.webp',
+    '/img_a/w106.webp',
+    '/img_a/w107.webp',
+    '/img_a/w108.webp',
+    '/img_a/w109.webp',
+    '/img_a/w110.webp',
+    '/img_a/w111.webp',
+    '/img_a/w112.webp',
+    '/img_a/w113.webp',
+    '/img_a/w114.webp',
+    '/img_a/w115.webp',
+    '/img_a/w116.webp',
+    '/img_a/w117.webp',
+    '/img_a/w118.webp',
+    '/img_a/w119.webp'
   ];
+
+  $: image = $page.url.hash;
 </script>
 
 <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -26,7 +30,7 @@
 
   <div class="carousel carousel-center rounded-box">
     {#each files as file, i}
-      <div id="item{i + 1}" class="carousel-item w-full">
+      <div id={(i + 1).toString()} class="carousel-item w-full">
         {#if i == 0}
           <img class="mx-auto" src={file} alt="akwarela nr {i}" />
         {:else}
@@ -38,7 +42,12 @@
 
   <div class="justify-center flex flex-wrap gap-2">
     {#each files as file, i}
-      <a href="#item{i + 1}" class="btn btn-xs">{i + 1}</a>
+      <a
+        href="#{i + 1}"
+        class="btn btn-xs {image === '#' + (i + 1).toString() || (image === '' && i === 0)
+          ? 'btn-active'
+          : ''}">{i + 1}</a
+      >
     {/each}
   </div>
 </div>
--- src/routes/kaligrafia/[...path]/+page.svelte
+++ src/routes/kaligrafia/[...path]/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { page } from '$app/stores';
+  
   export let data;
+
+  $: image = $page.url.hash;
 </script>
 
 <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -7,11 +11,11 @@
 
   <div class="carousel carousel-center rounded-box">
     {#each data.images as file, i}
-      <div id="item{i + 1}" class="carousel-item w-full">
+      <div id={(i + 1).toString()} class="carousel-item w-full">
         {#if i == 0}
-          <img class="mx-auto" src={file} alt="Obraz nr {i}" />
+          <img class="mx-auto" src={file} alt="Obraz nr {i + 1}" />
         {:else}
-          <img class="mx-auto" loading="lazy" src={file} alt="Obraz nr {i}" />
+          <img class="mx-auto" loading="lazy" src={file} alt="Obraz nr {i + 1}" />
         {/if}
       </div>
     {/each}
@@ -19,7 +23,12 @@
 
   <div class="justify-center flex flex-wrap gap-2">
     {#each data.images as file, i}
-      <a href="#item{i + 1}" class="btn btn-xs">{i + 1}</a>
+      <a
+        href="#{i + 1}"
+        class="btn btn-xs {image === '#' + (i + 1).toString() || (image === '' && i === 0)
+          ? 'btn-active'
+          : ''}">{i + 1}</a
+      >
     {/each}
   </div>
 </div>
