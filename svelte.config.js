c4120250f1eee0813ec77c6929169e5bd4828f8f
Marcin Dudek <manekd@gmail.com>
2024-10-20 16:46:46 +0200

    Adjust headers

--- svelte.config.js
+++ svelte.config.js
@@ -13,15 +13,9 @@ const config = {
         'default-src': ["'self'"],
         'img-src': ["'self'", 'data:'],
         'style-src': ["'self'", "'unsafe-inline'"],
-        'connect-src': ["'self'", 'https://cloudflareinsights.com', 'https://api.brevo.com', 'https://o4507021725138944.ingest.us.sentry.io'],
+        'connect-src': ["'self'", 'https://cloudflareinsights.com', 'https://api.brevo.com'],
         'script-src': ["'self'", 'https://static.cloudflareinsights.com'],
-        'report-to': ['https://o4507021725138944.ingest.sentry.io'],
-        'report-uri': ['https://o4507021725138944.ingest.sentry.io'],
         'worker-src': ["'self'", 'blob:'],
-      },
-      reportOnly: {
-        'report-uri': ['https://o4507021725138944.ingest.sentry.io'],
-        'report-to': ['csp-endpoint']
       }
     }
   },
