b1fe0baf202656ec282c7148e211cae2674ad211
Marcin Dudek <manekd@gmail.com>
2024-10-23 07:55:53 +0000

    Reset timer when manaulally changes picture

--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -1,18 +1,26 @@
 <script>
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
-  import { onMount } from 'svelte';
+  import { onDestroy, onMount } from 'svelte';
 
   $: current = $page.url.hash === '' ? 1 : Number($page.url.hash.substring(1));
 
-  // @ts-ignore
-  onMount(() => {
-    let x = setInterval(() => {
-      let c = (current % images.length) + 1;
-      goto('#' + c);
-    }, 5000);
+  /** @type {NodeJS.Timeout} */
+  let interval;
 
-    return () => clearInterval(x);
+  const changeImage = () => {
+    goto(`#${(current % images.length) + 1}`);
+  };
+
+  const handleClick = () => {
+    clearInterval(interval);
+    interval = setInterval(changeImage, 5000);
+    changeImage();
+  };
+
+  onMount(() => {
+    interval = setInterval(changeImage, 5000);
+    return () => clearInterval(interval);
   });
 
   export let images;
@@ -39,7 +47,7 @@
       <a
         href="#{i + 1}"
         class="btn btn-xs {current === i + 1 ? 'btn-active' : ''}"
-        on:click={() => goto('#' + (i + 1))}
+        on:click={handleClick}
         >{i + 1}
       </a>
     {/each}
