007dab5204ba3bd575d5cc9dc079392b0a2748a9
Marcin Dudek <manekd@gmail.com>
2024-10-25 11:28:45 +0200

    Change carousel

--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -1,28 +1,8 @@
 <script>
-  import { goto } from '$app/navigation';
-  import { page } from '$app/stores';
-  import { onMount } from 'svelte';
+  import Carousel from 'svelte-light-carousel';
 
-  $: current = $page.url.hash === '' ? 1 : Number($page.url.hash.substring(1));
-
-  /** @type {NodeJS.Timeout} */
-  let interval;
-
-  const changeImage = () => {
-    if (images.length > 0) {
-      goto(`#${(current % images.length) + 1}`);
-    }
-  };
-
-  const handleClick = () => {
-    clearInterval(interval);
-    interval = setInterval(changeImage, 5000);
-    changeImage();
-  };
-
-  onMount(() => {
-    interval = setInterval(changeImage, 5000);
-    return () => clearInterval(interval);
+  $: slides = Array.from(images, (v) => {
+    return { title: v };
   });
 
   /** @type {Array.<string>} */
@@ -34,26 +14,29 @@
 <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
   <h3 class="mb-4 text-4xl tracking-tight font-extrabold">{title}</h3>
 
-  <div class="carousel carousel-center rounded-box">
-    {#each images as file, i}
-      <div id={(i + 1).toString()} class="carousel-item w-full">
-        {#if i == 0}
-          <img class="mx-auto" src={file} alt="akwarela nr {i}" />
-        {:else}
-          <img class="mx-auto" loading="lazy" src={file} alt="akwarela nr {i}" />
-        {/if}
+  <Carousel {slides} oneAtTime={true} autoPlay={5}>
+    <div slot="slide" let:slide>
+      <div class="carousel-item">
+        <img src={slide.title} alt="Image for {title}" class="mx-auto" />
       </div>
-    {/each}
-  </div>
+    </div>
 
-  <div class="justify-center flex flex-wrap gap-2">
-    {#each images as file, i}
-      <a
-        href="#{i + 1}"
-        class="btn btn-xs {current === i + 1 ? 'btn-active' : ''}"
-        on:click={handleClick}
-        >{i + 1}
-      </a>
-    {/each}
-  </div>
+    <div
+      slot="dots"
+      let:dots
+      let:a11y
+      let:scrollTo
+      data-progress
+      class="justify-center flex flex-wrap py-4 gap-1"
+      {...a11y}
+    >
+      {#each dots as { a11y }, i}
+        <button
+          {...a11y}
+          on:click={() => scrollTo(i)}
+          class={'btn btn-sm btn-circle aria-[selected="true"]:bg-primary'}>{i + 1}</button
+        >
+      {/each}
+    </div>
+  </Carousel>
 </div>
